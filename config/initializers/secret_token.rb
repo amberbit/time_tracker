437752be05767ee8c506b7d14885c40bdc9a1145
Hubert <hubert.lepicki@amberbit.com>
2010-10-24 21:15:30 +0200

    initial rails app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTracker::Application.config.secret_token = 'd44834a53b3c1a2773029625bc5dbec8bf527df8296fd3ed1b6d79b20b37288d8e70340bcc081f785836f111a1e9b6fc7322f58e50546584d874682cc80bab63'
