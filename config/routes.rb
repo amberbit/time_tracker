cefb6c9242dca63debad377f11bd438cb3248013
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-10-24 22:02:40 +0200

    Devise installed. Sign up doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TimeTracker::Application.routes.draw do
+  get "welcome/index"
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +50,7 @@ TimeTracker::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/acceptance/user_registration_spec.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/acceptance_helper'
+
+feature "User Registration", %q{
+  In order to get access to the systen
+  As an anonymous user
+  I want to create an account
+} do
+
+  scenario "successful" do
+    visit homepage
+    click_link 'sign up'
+
+    fill_in 'Email', with: 'john.doe@example.org'
+    fill_in 'Password', with: 'foobar'
+    fill_in 'Password confirmation', with: 'foobar'
+    fill_in 'Pivotal Tracker API key', with: '12345678901234567890123456789012'
+    click_button 'Register'
+
+    page.should have_content("Welcome to Time Tracker!")
+    User.count.should eql(1)
+  end
+
+  scenario "unsuccessful" do
+    visit homepage
+    click_link 'sign up'
+
+    fill_in 'Email', with: 'john.doe@example.org'
+    fill_in 'Pivotal Tracker API key', with: '12345678901234567890123456789012'
+    click_button 'Register'
+
+    page.should have_content("Password can't be blank")
+    User.count.should eql(0)
+  end
+end
