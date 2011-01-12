8c5faf378ac62a475e7301d58ee184f8ca9459b1
Wojciech Piekutowski <wojciech@piekutowski.net>
2011-01-12 13:49:22 +0100

    Show users selector in time log entries report

--- app/helpers/time_log_entries_helper.rb
+++ app/helpers/time_log_entries_helper.rb
@@ -4,4 +4,11 @@ module TimeLogEntriesHelper
     project_options.unshift ["Any Project", nil]
     options_for_select(project_options, params[:project_id])
   end
+
+  def user_options
+    users = current_user.other_users
+    user_options = users.map { |u| [u.email, u.id] }
+    user_options.unshift ["Any User", nil]
+    options_for_select(user_options, params[:user_id])
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,25 @@ describe User do
       }.should raise_error
     end
   end
+
+  describe 'with projects' do
+    before :each do
+      @user1 = User.create! user_attributes(email: '1@1.com')
+      @user2 = User.create! user_attributes(email: '2@2.com')
+      @user3 = User.create! user_attributes(email: '3@3.com')
+
+      @project1 = Project.create! project_attributes
+      @project2 = Project.create! project_attributes
+
+      @project1.users << @user1
+      @project1.users << @user2
+      @project2.users << @user1
+      @project2.users << @user2
+      @project2.users << @user3
+    end
+
+    it "should return all users from his projects" do
+      @user1.other_users.map(&:email).should == [@user2, @user3].map(&:email)
+    end
+  end
 end
