a1a2e03c178479080cd2242f045ec8a1cd3265a0
Wojciech Piekutowski <wojciech@piekutowski.net>
2011-10-11 15:51:53 +0200

    Deploy from 1.4.0

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :user, "#{application}-#{stage}"
 set :group, "#{application}-#{stage}"
 set :deploy_env, 'production'
 set :deploy_to, "/home/#{user}/app"
-set :branch, 'v1.3.0'
+set :branch, 'v1.4.0'
