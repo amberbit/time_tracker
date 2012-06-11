3fe8b01871c74c1a59f97fbed48f3ce2a5dd5876
kamilbielawski <kamilbielawski1@gmail.com>
2012-06-11 13:19:56 +0200

    Eliminated delay when hiding accepted tasks on page load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $(".jump_to_url").live("change", function(event) {
+    window.location = $(this).val();
+  });
 
-$(".jump_to_url").live("change", function(event) {
- window.location = $(this).val();
+  $("#show_accepted").change(function() {
+    if($("#show_accepted").attr('checked'))
+      $(".accepted").show();
+    else
+      $(".accepted").hide();
+  }).change();
 });
-
-$("#show_accepted").change(function() {
-  if($("#show_accepted").attr('checked'))
-    $(".accepted").show();
-  else
-    $(".accepted").hide();
-}).change();
-
