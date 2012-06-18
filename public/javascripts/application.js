2789f589ea62e46dd61a953a2874f6b29840cbb7
kamilbielawski <kamilbielawski1@gmail.com>
2012-06-18 14:46:35 +0200

    Checkbox value is remembered between requests

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,27 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
 $(".jump_to_url").live("change", function(event) {
   window.location = $(this).val();
 });
 
+if( $.cookie('show_accepted') === 'true' )
+  $('#show_accepted').prop('checked', true);
+else 
+  $('#show_accepted').prop('checked', false);
+
 function show_accepted_changed() {
-  if($("#show_accepted").attr('checked'))
-    $(".accepted").show();
+  var checked = $('#show_accepted').is(':checked');
+  if(checked)
+    $('.accepted').show();
   else
-    $(".accepted").hide();
+    $('.accepted').hide();
+
+  $.cookie('show_accepted', checked, { path: '/' });
 }
-show_accepted_changed();
 
 $("#show_accepted").change(function() {
   show_accepted_changed();
-}).attr("onchange", function() {
-  show_accepted_changed();
 });
+
+show_accepted_changed();
--- spec/acceptance/downloading_tasks_spec.rb
+++ spec/acceptance/downloading_tasks_spec.rb
@@ -45,4 +45,18 @@ feature "Downloading Tasks", %q{
     page.should have_css('span', :text => "More power to shields", :visible => true)
     page.should have_css('span', :text => "Story Y", :visible => true)
   end
+
+  scenario "Checkbox value should be remembered between requests" do
+    fake_pivotal_api
+
+    sign_in_as "user@amberbit.com"
+
+    visit tasks_list
+    click_link "Refresh list of tasks"
+    select Project.first.name, from: 'project_id'
+    check 'show_accepted'
+
+    select Project.last.name, from: 'project_id'
+    find("input[type=checkbox]#show_accepted").should be_checked
+  end
 end
