8582c290146933d59ed9746503ba252e7c4299c7
kamilbielawski <kamilbielawski1@gmail.com>
2012-06-29 16:21:27 +0200

    Specs added

--- spec/acceptance/time_reporting_spec.rb
+++ spec/acceptance/time_reporting_spec.rb
@@ -20,6 +20,44 @@ feature "Time logging", %q{
     TimeLogEntry.first.should_not be_current
   end
 
+  scenario "Filtering time report by task" do
+    fake_pivotal_api
+    sign_in_as "user@amberbit.com"
+    visit tasks_list
+    click_link "Refresh list of tasks"
+    select Project.first.name, from: 'project_id'
+    click_link "Start work"
+    sleep 2
+    click_link "Stop work"
+
+    click_link 'Reports'
+
+    # read total time and convert it to seconds:
+    a=[1, 60, 3600]*2
+    time1 = within("#entries tfoot") { all("td")[1].text.split(/[:\.]/).map{|time| time.to_i*a.pop}.inject(&:+) }
+
+    select "task_id", from: "row_key"
+    select Project.first.name, from: 'project_id'
+    page.find("input[name=commit]").click
+  
+    within('#entries') do
+      page.should_not have_content "Story X"
+      page.should have_content "More power to shields"
+    end
+
+    select "Story X", from: 'task_id'
+    page.find("input[name=commit]").click
+
+    within('#entries') do
+      page.should_not have_content "Story X"
+      page.should_not have_content "More power to shields"
+    end
+
+    time2 = within("#entries tfoot") { all("td")[1].text.split(/[:\.]/).map{|time| time.to_i*a.pop}.inject(&:+) }
+
+    time1.should_not eq(time2)
+  end
+
   scenario "Seeing time spent on my tasks in given project" do
 
   end
