2a860b9380f72e99f0fe1c4aec0819c673a01edb
kamilbielawski <kamilbielawski1@gmail.com>
2012-07-04 00:07:27 +0200

    Specs fixed

--- spec/acceptance/time_logging_spec.rb
+++ spec/acceptance/time_logging_spec.rb
@@ -13,6 +13,7 @@ feature "Time logging", %q{
     visit tasks_list
     click_link "Refresh list of tasks"
     select Project.first.name, from: 'project_id'
+    check "show_accepted"
     click_link "Start work"
     TimeLogEntry.count.should eql(1)
     TimeLogEntry.first.should be_current
