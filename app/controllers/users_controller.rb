0f03510a7fe9ae1cfdef606b9eb41b26a73baff4
kamilbielawski <kamilbielawski1@gmail.com>
2012-07-08 20:25:42 +0200

    Value displayed in user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < AuthenticatedController
     user.set_employee_hourly_rate rate, Date.parse(params[:applies_from])
     redirect_to :back
   end
+
+  def get_total_earnings
+    user = User.find(params[:user_id])
+    session[:total_earnings] = user.total_earnings Date.parse(params[:from]), Date.parse(params[:to])
+    redirect_to :back
+  end
 end
