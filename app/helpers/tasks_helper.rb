b211a2e02a1d55f8ca102b0de0d7a6771dc6be7d
Bartek Sypytkowski <b.sypytkowski@gmail.com>
2012-10-07 22:25:22 +0200

    tasks sorts by accepted

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,15 @@
 module TasksHelper
+  def sort_by_accepted(tasks)
+    tasks.sort { |x,y|
+      if x.current_state == y.current_state
+        0
+      elsif x.current_state == 'accepted'
+        -1
+      elsif y.current_state == 'accepted'
+        1
+      else
+        0
+      end  
+    }
+  end
 end
